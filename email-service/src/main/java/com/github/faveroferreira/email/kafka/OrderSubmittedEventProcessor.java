a20736b238da5fa8ad622f9ea64394e6e0fc64fb
Guilherme Favero Ferreira <guifaveroferreira@gmail.com>
2021-06-17 14:17:51 -0300

    The End

--- email-service/src/main/java/com/github/faveroferreira/email/kafka/OrderSubmittedEventProcessor.java
+++ email-service/src/main/java/com/github/faveroferreira/email/kafka/OrderSubmittedEventProcessor.java
@@ -1,7 +1,17 @@
 package com.github.faveroferreira.email.kafka;
 
+import com.github.faveroferreira.order.dto.SubmittedOrderDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class OrderSubmittedEventProcessor {
+
+    @KafkaListener(topics = "order.submitted", groupId = "${spring.kafka.consumer.group-id}")
+    public void processEvent(SubmittedOrderDTO event) {
+        log.info("M=processEvent, message=envet = {}", event);
+    }
+
 }
--- order-service/src/main/java/com/github/faveroferreira/order/business/facade/ProcessSubmittedOrderFacade.java
+++ order-service/src/main/java/com/github/faveroferreira/order/business/facade/ProcessSubmittedOrderFacade.java
@@ -1,9 +1,11 @@
 package com.github.faveroferreira.order.business.facade;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.faveroferreira.order.business.service.OrderService;
 import com.github.faveroferreira.order.dto.SubmittedOrderDTO;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +13,9 @@ public class ProcessSubmittedOrderFacade {
 
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private KafkaTemplate<String, SubmittedOrderDTO> kafkaTemplate;
 
     public void processSubmittedOrder(SubmittedOrderDTO submittedOrderDTO) {
         final String persistedOrderId = orderService.persistSubmittedOrder(submittedOrderDTO);
@@ -20,6 +25,7 @@ public class ProcessSubmittedOrderFacade {
 
     @SneakyThrows
     private void publishOrderSubmittedEvent(SubmittedOrderDTO submittedOrderDTO) {
+        kafkaTemplate.send("order.submitted", submittedOrderDTO);
     }
 
 }
