d1e92c0e5e719a489806fe4ea74e4b09e2f93a05
Guilherme Favero Ferreira <guifaveroferreira@gmail.com>
2021-06-17 01:10:46 -0300

    esqueleto dos serviços

---
+++ order-service/src/test/java/com/github/faveroferreira/order/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.faveroferreira.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ payment-service/src/test/java/com/github/faveroferreira/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.faveroferreira.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
