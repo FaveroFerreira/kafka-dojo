f664a48c8048e36fab95df65d0c6faeec1e10812
Guilherme Favero Ferreira <guifaveroferreira@gmail.com>
2021-06-17 09:30:25 -0300

    Esqueleto do producer

--- order-service/src/main/java/com/github/faveroferreira/order/business/facade/ProcessSubmittedOrderFacade.java
+++ order-service/src/main/java/com/github/faveroferreira/order/business/facade/ProcessSubmittedOrderFacade.java
@@ -1,11 +1,9 @@
 package com.github.faveroferreira.order.business.facade;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.faveroferreira.order.business.service.OrderService;
 import com.github.faveroferreira.order.dto.SubmittedOrderDTO;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +11,6 @@ public class ProcessSubmittedOrderFacade {
 
     @Autowired
     private OrderService orderService;
-
-    @Autowired
-    private KafkaTemplate<String, String> kafkaTemplate;
 
     public void processSubmittedOrder(SubmittedOrderDTO submittedOrderDTO) {
         final String persistedOrderId = orderService.persistSubmittedOrder(submittedOrderDTO);
@@ -25,8 +20,6 @@ public class ProcessSubmittedOrderFacade {
 
     @SneakyThrows
     private void publishOrderSubmittedEvent(SubmittedOrderDTO submittedOrderDTO) {
-        String json = new ObjectMapper().writeValueAsString(submittedOrderDTO);
-        kafkaTemplate.send("order.submitted", json);
     }
 
 }
