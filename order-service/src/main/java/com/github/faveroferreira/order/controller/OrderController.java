072dfe9da6770374b96c1041b73ea0360eedffa9
Guilherme Favero Ferreira <guifaveroferreira@gmail.com>
2021-06-17 03:27:29 -0300

    Esqueleto do Producer

--- order-service/src/main/java/com/github/faveroferreira/order/controller/OrderController.java
+++ order-service/src/main/java/com/github/faveroferreira/order/controller/OrderController.java
@@ -2,10 +2,13 @@ package com.github.faveroferreira.order.controller;
 
 import com.github.faveroferreira.order.business.facade.ProcessSubmittedOrderFacade;
 import com.github.faveroferreira.order.dto.SubmittedOrderDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class OrderController {
 
@@ -13,7 +16,8 @@ public class OrderController {
     private ProcessSubmittedOrderFacade submittedOrderProcessor;
 
     @PostMapping("/order/submit")
-    public void receiveSubmittedOrder(SubmittedOrderDTO submittedOrderDTO) {
+    public void receiveSubmittedOrder(@RequestBody SubmittedOrderDTO submittedOrderDTO) {
+        log.info("M=receiveSubmittedOrder, message=Received submitted order: {}", submittedOrderDTO);
         submittedOrderProcessor.processSubmittedOrder(submittedOrderDTO);
     }
 
